ee826ec355b0237453db937573a5081d0c19de66
Erick Teowarang <erick@worthycause.com.au>
2022-07-03 19:45:55 +1000

    Add animation to the card grid

--- src/components/molecules/CardGrid/index.tsx
+++ src/components/molecules/CardGrid/index.tsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
+import { AnimatePresence, motion } from 'framer-motion';
 
-import Box from 'components/atoms/Box';
 import Card, { CardProps } from 'components/atoms/Card';
 import Container from 'components/atoms/Container';
 import Flex from 'components/atoms/Flex';
+
+import { media } from 'theme/media';
 
 const CardGridContainer = styled(Container)`
   margin-bottom: ${({ theme }) => theme.space[7]};
 `;
 
+const CardContainer = styled(motion.div)`
+  width: 100%;
+
+  @media ${media.small} {
+    width: 31.2%;
+  }
+`
+
 const CardGrid = ({ cards }: { cards: Array<CardProps> }) => (
   <CardGridContainer>
     <Flex gap={9} wrap>
-      {cards.map((card) => (
-        <Box width="31.2%">
-          <Card {...card} fullWidth />
-        </Box>
-      ))}
+      <AnimatePresence initial={false}>
+        {cards.map((card, i) => (
+          <CardContainer
+            key={card.heading}
+            layout
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.6 }}
+          >
+            <Card key={i} {...card} fullWidth />
+          </CardContainer>
+        ))}
+      </AnimatePresence>
     </Flex>
   </CardGridContainer>
 );

73372571be7d38f961437e27465308a877beb16f
Erick Teowarang <eteowarang@atlassian.com>
2024-08-17 00:18:01 +1000

    Test server data running

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,63 +4,46 @@ import Layout from 'components/organisms/Layout';
 import ContentFilter from 'components/organisms/ContentFilter';
 import CardGrid from 'components/molecules/CardGrid';
 
-const PortfolioItemPage = () => {
+const PortfolioItemPage = ({ serverData }) => {
   const [cards, setCards] = useState([]);
   const [activeFilter, setActiveFilter] = useState('All');
-  const { allPortfolioItem } = useStaticQuery(graphql`
-    query allPortfolioItemQuery {
-      allPortfolioItem {
-        nodes {
-          title
-          excerpt
-          image {
-            id
-            gatsbyImageData
-            alt
-            url
-          }
-          uri
-          categories {
-            name
-          }
-        }
-      }
-    }
-  `);
+  console.log('serverData', serverData);
 
-  const getCategories = () => {
-    let allCategories = ['All'];
-    allPortfolioItem?.nodes?.forEach((node) => {
-      node.categories.forEach((category) => {
-        if (!allCategories.includes(category.name)) {
-          allCategories.push(category.name);
-        }
-      });
-    });
+  // const getCategories = () => {
+  //   let allCategories = ['All'];
+  //   allPortfolioItem?.nodes?.forEach((node) => {
+  //     node.categories.forEach((category) => {
+  //       if (!allCategories.includes(category.name)) {
+  //         allCategories.push(category.name);
+  //       }
+  //     });
+  //   });
 
-    return allCategories;
-  };
+  //   return allCategories;
+  // };
 
-  const getCards = () => {
-    let allPortfolioItems = allPortfolioItem.nodes;
-    if (activeFilter !== 'All') {
-      allPortfolioItems = allPortfolioItems.filter((node) =>
-        node.categories.some((category) => category.name === activeFilter)
-      );
-    }
+  // const getCards = () => {
+  //   let allPortfolioItems = allPortfolioItem.nodes;
+  //   if (activeFilter !== 'All') {
+  //     allPortfolioItems = allPortfolioItems.filter((node) =>
+  //       node.categories.some((category) => category.name === activeFilter)
+  //     );
+  //   }
 
-    return allPortfolioItems.map((node) => ({
-      heading: node.title,
-      content: node.excerpt,
-      image: node.image,
-      tags: node.categories.map((category) => category.name),
-      link: node.uri,
-    }));
-  };
+  //   if (allPortfolioItems) {
+  //     return allPortfolioItems?.map((node) => ({
+  //       heading: node.title,
+  //       content: node.excerpt,
+  //       image: node.image,
+  //       tags: node.categories.map((category) => category.name),
+  //       link: node.uri,
+  //     }));
+  //   }
+  // };
 
-  useEffect(() => {
-    setCards(getCards());
-  }, [activeFilter]);
+  // useEffect(() => {
+  //   setCards(getCards());
+  // }, [activeFilter]);
 
   return (
     <Layout
@@ -70,13 +53,13 @@ const PortfolioItemPage = () => {
         footerCtaHeading: `Let's talk digital`,
       }}
     >
-      <ContentFilter
+      {/* <ContentFilter
         heading="Selected work"
         filters={getCategories()}
         setActiveFilter={setActiveFilter}
         activeFilter={activeFilter}
       />
-      {cards.length && (
+      {cards?.length && (
         <CardGrid
           cards={cards}
           flexStart
@@ -88,9 +71,59 @@ const PortfolioItemPage = () => {
             borderRadius: '20px',
           }}
         />
-      )}
+      )} */}
     </Layout>
   );
 };
 
 export default PortfolioItemPage;
+
+export async function getServerData() {
+  try {
+    const res = await fetch('https://admin.thegoodeggs.com/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+            {
+              portfolioItems(where: { orderby: { field: MENU_ORDER, order: ASC } }) {
+                nodes {
+                  title
+                  excerpt
+                  featuredImage {
+                    node {
+                      id
+                      altText
+                      sourceUrl					
+                    }
+                  }
+                  uri
+                  categories {
+                    nodes {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+        `,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`Response failed`);
+    }
+
+    return {
+      props: await res.json(),
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      headers: {},
+      props: {},
+    };
+  }
+}

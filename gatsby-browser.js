0f0c38ddb0202bcd132b007dbdc3c28a71a666a2
Erick Teowarang <eteowarang@atlassian.com>
2024-10-14 12:31:53 +1100

    Hardcode URL

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,22 @@
-import * as React from "react";
-import fetch from "cross-fetch";
-import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from "@apollo/client";
+import * as React from 'react';
+import fetch from 'cross-fetch';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+} from '@apollo/client';
 import { AnimatePresence } from 'framer-motion';
-import { DataProvider } from "context/DataProvider";
+import { DataProvider } from 'context/DataProvider';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
-import './overrides.css'
+import './overrides.css';
 
 export const wrapRootElement = ({ element }) => {
   const client = new ApolloClient({
     link: new HttpLink({
-      uri: process.env.WPGRAPHQL_URL,
+      uri: 'https://admin.thegoodeggs.com.au/graphql',
       fetch,
     }),
     cache: new InMemoryCache(),
@@ -20,7 +25,7 @@ export const wrapRootElement = ({ element }) => {
   return <ApolloProvider client={client}>{element}</ApolloProvider>;
 };
 
-export const wrapPageElement = ({element, props}) => (
+export const wrapPageElement = ({ element, props }) => (
   <DataProvider value={props}>
     <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>
   </DataProvider>

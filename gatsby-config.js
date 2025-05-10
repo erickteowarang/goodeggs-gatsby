1127a962c8590b0804c20ca459fb8fece975f70c
Erick Teowarang <erick.teowarang@hotmail.com>
2025-05-11 00:06:01 +1000

    Adjust the gatsby-config value

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,9 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         url: process.env.WPGRAPHQL_URL,
+        schema: {
+          perPage: 75,
+        },
       },
     },
     'gatsby-plugin-styled-components',
